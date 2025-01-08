bcaecfd5455c4dc086c09c2cb9762279126c1dca
Sören Michaelis <soeren.michaelis11@gmail.com>
2025-01-08 11:11:10 +0100

    provide database migration and connection
    Signed-off-by: Sören Michaelis <soeren.michaelis11@gmail.com>

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"errors"
 	"github.com/Soemii/golang-template/internal/inbound/web"
+	"github.com/Soemii/golang-template/internal/outbound/datastore"
+	"github.com/Soemii/golang-template/internal/outbound/datastore/migrations"
+	"github.com/golang-migrate/migrate/v4"
 	"go.uber.org/fx"
+	"io/fs"
 	"net/http"
 )
 
@@ -12,7 +17,19 @@ func main() {
 			web.NewServer,
 			fx.Annotate(web.NewMux, fx.ParamTags(`group:"route"`)),
 		),
+		fx.Provide(
+			fx.Annotate(migrations.NewMigrationFiles, fx.ResultTags(`name:"migrationFiles"`), fx.As(new(fs.FS))),
+			fx.Annotate(migrations.NewMigration, fx.ParamTags(`name:"migrationFiles"`)),
+			datastore.NewDB,
+		),
 		fx.Invoke(func(*http.Server) {}),
+		fx.Invoke(func(migration *migrate.Migrate) error {
+			err := migration.Up()
+			if !errors.Is(err, migrate.ErrNoChange) {
+				return err
+			}
+			return nil
+		}),
 	).Run()
 }
 
