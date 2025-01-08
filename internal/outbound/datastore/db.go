983a7985d8e5edb5af1a3c9fc280848b4df77b14
Sören Michaelis <soeren.michaelis11@gmail.com>
2025-01-08 11:09:33 +0100

    added db connection
    Signed-off-by: Sören Michaelis <soeren.michaelis11@gmail.com>

--- internal/outbound/datastore/db.go
+++ internal/outbound/datastore/db.go
@@ -1,1 +1,18 @@
 package datastore
+
+import (
+	"database/sql"
+	_ "github.com/jackc/pgx/v5/stdlib"
+	"os"
+)
+
+func NewDB() (*sql.DB, error) {
+	d, err := sql.Open("pgx", os.Getenv("DB_DSN"))
+	if err != nil {
+		return nil, err
+	}
+	if err := d.Ping(); err != nil {
+		return nil, err
+	}
+	return d, nil
+}
